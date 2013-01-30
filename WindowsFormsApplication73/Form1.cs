b434aeb40a515f1a21fae80d95a6a6f0d3d71dbf
unknown <Dax@DAX-PC.corp.sparx.net>
2013-01-29 18:28:57 -0600

    fix

--- WindowsFormsApplication73/Form1.cs
+++ WindowsFormsApplication73/Form1.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication73
@@ -16,9 +10,11 @@ namespace WindowsFormsApplication73
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        void button1_Click(object sender, EventArgs e)
         {
-            var result = new Form2().ShowWithTimeout(TimeSpan.FromSeconds(3));
+            var form2 = new Form2();
+            var timeout = TimeSpan.FromSeconds(3);
+            var result = form2.ShowWithTimeout(timeout);
             MessageBox.Show(result.ToString());
         }
     }
--- WindowsFormsApplication73/Program.cs
+++ WindowsFormsApplication73/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication73
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
